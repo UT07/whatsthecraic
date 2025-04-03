82911f3addf2c03e06cd061e61d8710da70e331b
Utkarsh Singh <254utkarsh@gmail.com>
2025-04-03 15:13:15 +0100

    fixing the aggregator service URL

--- gigfinder-app/src/services/aggregatorAPI.js
+++ gigfinder-app/src/services/aggregatorAPI.js
@@ -4,20 +4,18 @@ const API_BASE = 'https://ec2-18-201-228-48.eu-west-1.compute.amazonaws.com';
 
 const aggregatorAPI = {
   searchEvents: async (filters) => {
-    // Remove keys with empty or whitespace-only values
     const validFilters = Object.keys(filters)
       .filter(key => filters[key] && filters[key].trim() !== '')
       .reduce((acc, key) => {
         acc[key] = filters[key];
         return acc;
       }, {});
-      
-    // Use the correct endpoint for aggregator searches
-    const url = `/api/gigs`;
+
+    const url = `${API_BASE}/api/gigs`; // 🔥 absolute URL
     console.log("Search URL:", url, "with params:", validFilters);
 
     const response = await axios.get(url, { params: validFilters });
-    return response.data;
+    return response.data.gigs; // .gigs because your API wraps it like { gigs: [...] }
   }
 };
 
